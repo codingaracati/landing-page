7c5c6cbc7b979aeb38bf20f3469e893ece5ae7ad
Alberto Zaranza <albertozaranza@outlook.com>
2024-03-18 19:22:17 -0300

    fix: base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://codingaracati.github.io',
+  base: '/landing-page',
   integrations: [react(), tailwind()]
 });
